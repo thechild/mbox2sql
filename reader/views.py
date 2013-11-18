a2663b003f28d91aca45ae8476f38773ac539a67
Chris Child <thechild@gmail.com>
2013-11-17 22:55:50 -0800

    Lots of formatting, including a non-working search field

--- reader/views.py
+++ reader/views.py
@@ -6,6 +6,11 @@ messages_per_page = 25
 
 # Create your views here.
 
+def get_default_context(request):
+    unread_message_count = Message.objects.count() ## clearly need to fix this
+    context = {'unread_message_count': unread_message_count }
+    return context
+
 def message_list(request):
     message_list = Message.objects.all().order_by('-sent_date')
     paginator = Paginator(message_list, messages_per_page)
@@ -18,18 +23,26 @@ def message_list(request):
     except EmptyPage:
         messages = paginator.page(paginator.num_pages)
 
-    return render_to_response('message_list.html', {"message_list": messages })
+    context = get_default_context(request)
+    context.update({'message_list': messages})
+
+    return render_to_response('message_list.html', context)
 
 def view_message(request, message_id, text=False):
     message = Message.objects.get(id=message_id)
-    return render_to_response('message.html', {'message': message, 'show_text': text})
+    context = get_default_context(request)
+    context.update({ 'message': message, 'show_text': text })
+
+    return render_to_response('message.html', context)
 
 def view_message_text(request, message_id):
     return view_message(request, message_id, text=True)
 
 def view_person(request, person_id):
     person = Address.objects.get(id=person_id)
-    return render_to_response('person.html', {'person': person})
+    context = get_default_context(request)
+    context.update({ 'person': person })
+    return render_to_response('person.html', context)
 
 def view_group(request, group_hash):
     pass
