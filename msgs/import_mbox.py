aaae068b8a34a3eb8f8ebdbb7b477a38bf01e66e
Chris Child <thechild@gmail.com>
2013-12-09 10:37:40 -0800

    handle related images a little better

--- msgs/import_mbox.py
+++ msgs/import_mbox.py
@@ -95,8 +95,11 @@ def message_exists(message_id):
 # doesn't handle multiple text or html parts well right now - just takes the first one - could just do an append?
 # if encounters multipart content, it recurses on all the parts
 # if Content-Type is not multipart/alternative, multipart/mixed, multipart/related, text/plain or text/html, assumes it's an attachment and reacts accordingly
-def fill_in_message_content(message, content):
-    if content.get_content_type() in ('multipart/alternative', 'multipart/mixed', 'multipart/related'):
+def fill_in_message_content(message, content, related=False):
+    if content.get_content_type() == 'multipart/related':
+        for part in content.get_payload():
+            message = fill_in_message_content(message, part, related=True)
+    elif content.get_content_type() in ('multipart/alternative', 'multipart/mixed'):
         for part in content.get_payload():
             message = fill_in_message_content(message, part)
     elif content.get_content_type() == 'text/plain':
@@ -110,7 +113,7 @@ def fill_in_message_content(message, content):
     else:
         # assume it's an attachment (this may be a bad idea) and save it to disk
         message.save() # this feels a bit like cheating, but we need an id for the message now
-        handle_attachment(message, content)
+        handle_attachment(message, content, related)
     return message
 
 
@@ -125,12 +128,13 @@ def get_charset(message, default='ascii'):
     return default
 
 # saves content as a file and creates an Attachment connected to message
-def handle_attachment(message, content):
+def handle_attachment(message, content, related):
     print "saving attachment of type %s from message %d " % (content.get_content_type(), message.id)
     a = models.Attachment()
     a.filename = content.get_filename()
     a.content_type = content.get_content_type()
     a.stored_location = os.path.join(files_dir, str(message.id), a.filename) # probably want to fix this too
+    a.mime_related = related
     # load the file
     file_content = content.get_payload(decode=1)
     a.file_md5 = hashlib.md5(file_content).hexdigest() # again, probably a better way to do this than all in memory
