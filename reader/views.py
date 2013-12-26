cba96805fb7ca32d0e4d235da7638f8b3e4584f5
Chris Child <thechild@gmail.com>
2013-12-25 20:47:29 -0800

    cleaned up

--- reader/views.py
+++ reader/views.py
@@ -5,8 +5,20 @@ from msgs.groups import Group, group_from_url_representation, groups_including_p
 
 messages_per_page = 25
 
-# Create your views here.
+# helper function to get information needed by the base.html template
+def get_default_context(request):
+    unread_message_count = len(get_all_message_threads()) ## clearly need to fix this
+    unread_conversation_count = Conversation.objects.count() ## and this
+    context = {'unread_message_count': unread_message_count,
+               'unread_conversation_count': unread_conversation_count }
+    return context
 
+def get_context_with(request, dict):
+    context = get_default_context(request)
+    context.update(dict)
+    return context
+
+# returns a raw message (used for iframes) in either html or text
 def raw_message(request, message_id, text=False):
     message = Message.objects.get(id=message_id)
     context = get_default_context(request)
@@ -20,12 +32,9 @@ def raw_message(request, message_id, text=False):
 def raw_message_text(request, message_id):
     return raw_message(request, message_id, text=True)
 
-def get_default_context(request):
-    unread_message_count = len(get_all_message_threads()) ## clearly need to fix this
-    unread_conversation_count = Conversation.objects.count() ## and this
-    context = {'unread_message_count': unread_message_count,
-               'unread_conversation_count': unread_conversation_count }
-    return context
+##################
+### List Pages ###
+##################
 
 def conversation_list(request):
     message_list = get_sorted_conversations()
@@ -44,16 +53,6 @@ def conversation_list(request):
 
     return render_to_response('conversation_list.html', context)
 
-def view_conversation(request, conversation_id, text=False):
-    conversation = Conversation.objects.get(id=conversation_id)
-    context = get_default_context(request)
-    context.update({ 'conversation': conversation, 'show_text': text })
-
-    return render_to_response('conversation.html', context)
-
-def view_conversation_text(request, conversation_id):
-    return view_conversation(request, conversation_id, text=True)
-
 def message_list(request):
     message_list = get_all_message_threads()
     paginator = Paginator(message_list, messages_per_page)
@@ -66,20 +65,8 @@ def message_list(request):
     except EmptyPage:
         messages = paginator.page(paginator.num_pages)
 
-    context = get_default_context(request)
-    context.update({'message_list': messages})
-
+    context = get_context_with(request, {'message_list': messages })
     return render_to_response('message_list.html', context)
-
-def view_message(request, message_id, text=False):
-    message = Message.objects.get(id=message_id)
-    context = get_default_context(request)
-    context.update({ 'message': message, 'show_text': text })
-
-    return render_to_response('message.html', context)
-
-def view_message_text(request, message_id):
-    return view_message(request, message_id, text=True)
 
 def people_list(request):
     p_list = Address.objects.exclude(address='cchild@redpoint.com') # well that's a good hack
@@ -93,27 +80,43 @@ def people_list(request):
 
     sp_list = sorted(p_list, key=latest_convo_date, reverse=True) # should do this in db
 
-    context = get_default_context(request)
-    context.update({'people_list': sp_list })
+    context = get_context_with(request, {'people_list': sp_list })
     return render_to_response('people_list.html', context)
-
-def view_person(request, person_id):
-    person = Address.objects.get(id=person_id)
-    context = get_default_context(request)
-    context.update({ 'person': person })
-    return render_to_response('person.html', context)
 
 def group_list(request):
     person = Address.objects.get(address="cchild@redpoint.com") # how's that for a hard code
     groups = groups_including_person(person)
-    context = get_default_context(request)
-    context.update({ 'groups': groups })
+    context = get_context_with(request, {'groups': groups })
     return render_to_response('group_list.html', context)
+
+###############################
+### Individual Object Views ###
+###############################
+
+def view_conversation(request, conversation_id, text=False):
+    conversation = Conversation.objects.get(id=conversation_id)
+
+    context = get_context_with(request, {'conversation': conversation, 'show_text': text })
+    return render_to_response('conversation.html', context)
+
+def view_conversation_text(request, conversation_id):
+    return view_conversation(request, conversation_id, text=True)
+
+def view_message(request, message_id, text=False):
+    message = Message.objects.get(id=message_id)
+    context = get_context_with(request, {'message': message, 'show_text': text })
+    return render_to_response('message.html', context)
+
+def view_message_text(request, message_id):
+    return view_message(request, message_id, text=True)
+
+def view_person(request, person_id):
+    person = Address.objects.get(id=person_id)
+    context = get_context_with(request, {'person': person})
+    return render_to_response('person.html', context)
 
 def view_group(request, group_id):
     group = group_from_conversation(Conversation.objects.get(id=group_id))
-    #group = group_from_url_representation(group_members)
-    context = get_default_context(request)
-    context.update({ 'group': group })
+    context = get_context_with(request, {'group': group})
     return render_to_response('view_group.html', context)
 
