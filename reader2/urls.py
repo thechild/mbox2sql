9ce9b0f6aae22a960fcfb49c8862485f796212a8
Chris Child <thechild@gmail.com>
2014-11-24 14:23:53 -0800

    hid unimplemented views

--- reader2/urls.py
+++ reader2/urls.py
@@ -7,12 +7,12 @@ from django.views.generic.base import RedirectView
 from reader2 import views
 
 urlpatterns = patterns('',
-    url(r'group/(?P<group_id>\d+)/$', views.view_group, name="view_group"),
-    url(r'person/(?P<person_id>\d+)/$', views.view_person, name="view_person"),
-    url(r'todo/(?P<todo_id>\d+)/$', views.view_todo, name="view_todo"),
-    url(r'conversation/(?P<conversation_id>\d+)/$', views.view_conversation, name="view_conversation"),
+    #url(r'group/(?P<group_id>\d+)/$', views.view_group, name="view_group"),
+    #url(r'person/(?P<person_id>\d+)/$', views.view_person, name="view_person"),
+    #url(r'todo/(?P<todo_id>\d+)/$', views.view_todo, name="view_todo"),
+    #url(r'conversation/(?P<conversation_id>\d+)/$', views.view_conversation, name="view_conversation"),
 
     url(r'incoming/$', views.incoming_list, name="incoming_list"),
-    url(r'todos/$', views.todo_list, name="todo_list"),
-    url(r'other/$', views.other_list, name="other_list"),
+    #url(r'todos/$', views.todo_list, name="todo_list"),
+    #url(r'other/$', views.other_list, name="other_list"),
 )
