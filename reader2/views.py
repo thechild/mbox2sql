99fe079282dd9e930e2e2056eb6c47c03c6a5f1b
Chris Child <thechild@gmail.com>
2014-11-24 12:30:44 -0800

    start of inbox views

--- reader2/views.py
+++ reader2/views.py
@@ -1,1 +1,46 @@
+from msgs2.models import Person, Address, Account, Message, MessageBody, MessageFlag, Header, Attachment, ToDo
+
 # Create your views here.
+
+def get_inbox(account=None):
+	full_inbox = Message.objects.filter(flags__flag=MessageFlag.INBOX_FLAG)
+	if account:
+		return full_inbox.filter(account=account)
+	else:
+		return full_inbox
+
+# have I ever sent an email to this person before
+def is_sender_legit(email, account=None):
+	if not account:
+		accounts = Account.objects.all()
+		return Message.objects.filter(sender__email__in=[account.address for account in accounts],
+									  members__addresses__email=email).exists()
+	else:
+		return Message.objects.filter(sender__email=account.address,
+									  members__addresses__email=email).exists()
+
+
+def is_repeat_sender(email, account=None):
+	return Address.objects.get(email=email).sent_messages.count() > 1
+
+
+def parse_inbox(account=None):
+	inbox = get_inbox(account)
+
+	primary_inbox = []
+	new_sender_inbox = []
+	repeat_sender_inbox = []
+
+	# I bet the database can do a lot of this, but that's an optimization for later
+	for m in inbox:
+		if is_sender_legit(m.sender.email):
+			primary_inbox.append(m)
+		elif is_repeat_sender(m.sender.email):
+			repeat_sender_inbox.append(m)
+		else:
+			new_sender_inbox.append(m)
+
+	return {'primary': primary_inbox,
+			'new_sender': new_sender_inbox,
+			'repeat_sender': repeat_sender_inbox}
+
