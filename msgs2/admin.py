3d970a8bc97e368e0e2827250ce0a5bd4f690197
Chris Child <thechild@gmail.com>
2014-11-24 14:22:58 -0800

    Registered models

--- msgs2/admin.py
+++ msgs2/admin.py
@@ -1,1 +1,14 @@
+from django.contrib import admin
+from msgs2.models import Person, Address, Account, Message, MessageFlag, Header, MessageBody, Attachment, ToDo
+
 # Register your models here.
+
+#admin.site.register(Person)
+#admin.site.register(Address)
+#admin.site.register(Account)
+#admin.site.register(Message)
+#admin.site.register(MessageFlag)
+#admin.site.register(Header)
+#admin.site.register(MessageBody)
+#admin.site.register(Attachment)
+#admin.site.register(ToDo)
