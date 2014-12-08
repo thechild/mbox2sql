6b321d27cad205aea398454761fb2beb66183c85
Chris Child <thechild@gmail.com>
2014-12-08 13:45:39 -0800

    rearranged, also support for not including bodies

--- jsonapi/views.py
+++ jsonapi/views.py
@@ -1,72 +1,58 @@
-from msgs2.models import Person, Address, Account, Message, MessageBody, MessageFlag, Header, Attachment, ToDo
+from msgs2 import inbox
+from msgs2.models import Message
 import json
 from django.http import HttpResponse
+from django.core.urlresolvers import reverse
 
 
 # JSON API VIEWS
 
 def primary_inbox(request):
-	incoming_inbox = parse_inbox()['primary']
-	return inbox_as_json_response(incoming_inbox)
+	incoming_inbox = inbox.parse_inbox()['primary']
+	return inbox_as_json_response(incoming_inbox, request.GET.get('bodies'))
 
 def mass_inbox(request):
-	mass_inbox = parse_inbox()['repeat_sender']
-	return inbox_as_json_response(mass_inbox)
+	mass_inbox = inbox.parse_inbox()['repeat_sender']
+	return inbox_as_json_response(mass_inbox, request.GET.get('bodies'))
 
 def new_inbox(request):
-	new_inbox = parse_inbox()['new_sender']
-	return inbox_as_json_response(new_inbox)
+	new_inbox = inbox.parse_inbox()['new_sender']
+	return inbox_as_json_response(new_inbox, request.GET.get('bodies'))
+
+def get_message(request, message_id):
+	try:
+		message = Message.objects.get(id=message_id)
+	except Message.DoesNotExist:
+		return error_as_json_response('No message with the requested ID exists.')
+	r = {'status': 'Success',
+ 		 'message_id': message.id,
+		 'message': message.as_json() }
+	return get_json_response(r)
+
+
+
 
 ### Helper functions ###
 
-def inbox_as_json_response(inbox):
-	json_inbox = [m.as_json() for m in inbox]
-	r = {'Status': 'Success',
-		 'Inbox': json_inbox}
-	return HttpResponse(json.dumps(r, default=date_handler), mimetype='application/json')
+def error_as_json_response(error_message):
+	r = {'status': 'Error',
+		 'message': error_message}
+	return get_json_response(r, status=404)
+
+def inbox_as_json_response(inbox, include_message_bodies=True):
+	json_inbox = [m.as_json(include_message_bodies) for m in inbox]
+	for m in json_inbox:
+		m['href'] = reverse('jsonapi:message', args=(m['message_id'], ))
+	r = {'status': 'Success',
+		 'count': len(json_inbox),
+		 'inbox': json_inbox}
+	return get_json_response(r)
+	
+def get_json_response(r_json, status=200):
+	return HttpResponse(json.dumps(r_json, default=date_handler),
+						content_type='application/json',
+						status=status)
 
 def date_handler(obj):
 	return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
-def get_inbox(account=None):
-	full_inbox = Message.objects.filter(flags__flag=MessageFlag.INBOX_FLAG).order_by('-sent_date')
-	if account:
-		return full_inbox.filter(account=account)
-	else:
-		return full_inbox
-
-# have I ever sent an email to this person before
-def is_sender_legit(email, account=None):
-	if not account:
-		accounts = Account.objects.all()
-		return Message.objects.filter(sender__email__in=[account.email_address() for account in accounts],
-									  members__addresses__email=email).exists()
-	else:
-		return Message.objects.filter(sender__email=account.email_address(),
-									  members__addresses__email=email).exists()
-
-
-def is_repeat_sender(email, account=None):
-	return Address.objects.get(email=email).sent_messages.count() > 1
-
-
-# gets the inbox and splits it - should make this all DB calls if possible
-def parse_inbox(account=None):
-	inbox = get_inbox(account)
-
-	primary_inbox = []
-	new_sender_inbox = []
-	repeat_sender_inbox = []
-
-	# I bet the database can do a lot of this, but that's an optimization for later
-	for m in inbox:
-		if is_sender_legit(m.sender.email):
-			primary_inbox.append(m)
-		elif is_repeat_sender(m.sender.email):
-			repeat_sender_inbox.append(m)
-		else:
-			new_sender_inbox.append(m)
-
-	return {'primary': primary_inbox,
-			'new_sender': new_sender_inbox,
-			'repeat_sender': repeat_sender_inbox}
