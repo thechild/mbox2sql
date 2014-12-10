cf9cd4609f52d1732cd8276d654b137798de6cb0
Chris Child <thechild@gmail.com>
2014-12-10 14:26:24 -0800

    off by one

--- exchange.py
+++ exchange.py
@@ -106,7 +106,7 @@ class ExchangeMail():
                 raise Exception("Couldn't find folder named %s" % folder_name)
             folder_id, item_count = folder
 
-        for i in xrange(int(item_count) / step):
+        for i in xrange(int(item_count) / step + 1):
             # get the list of step items
             body = exml.get_folder_items_range(folder_id=folder_id, format=u"IdOnly",
                 max_entries=step, offset=i * step)
