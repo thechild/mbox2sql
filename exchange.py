48d65776ff8a8c9ef2e170f75cadefe6ba168104
Chris Child <thechild@gmail.com>
2014-12-10 13:00:19 -0800

    tabs to spaces

--- exchange.py
+++ exchange.py
@@ -6,210 +6,210 @@ import xmltodict
 import ipdb
 
 def pxml(xml):
-	print(etree.tostring(xml, pretty_print=True))
+    print(etree.tostring(xml, pretty_print=True))
 
 class MessageItem():
-	def __init__(self, folder_id, item_id, mail):
-		self._folder_id = folder_id
-		self._item_id = item_id
-		self._mail = mail
+    def __init__(self, folder_id, item_id, mail):
+        self._folder_id = folder_id
+        self._item_id = item_id
+        self._mail = mail
 
-	@property
-	def folder_id(self):
-	    return self._folder_id
+    @property
+    def folder_id(self):
+        return self._folder_id
 
-	@property
-	def item_id(self):
-		return self._item_id
+    @property
+    def item_id(self):
+        return self._item_id
 
-	def message(self):
-		if not self._message:
-			root = self._mail.get_item_nck(self._item_id)
-			self._message = self._mail.service.send(root)
-		return self._message
+    def message(self):
+        if not self._message:
+            root = self._mail.get_item_nck(self._item_id)
+            self._message = self._mail.service.send(root)
+        return self._message
 
 
 class ExchangeMail():
-	def __init__(self, url, username, password):
-		self.url = url
-		self.username = username
-		self._get_service(url, username, password)
+    def __init__(self, url, username, password):
+        self.url = url
+        self.username = username
+        self._get_service(url, username, password)
 
 
-	def _get_service(self, url, username, password):
-		connection = ExchangeNTLMAuthConnection(url=url, username=username, password=password)
-		service = Exchange2010Service(connection)
-		self.service = service
+    def _get_service(self, url, username, password):
+        connection = ExchangeNTLMAuthConnection(url=url, username=username, password=password)
+        service = Exchange2010Service(connection)
+        self.service = service
 
 
-	# should be in soap_request
-	def get_folder_items(self, format=u"Default", d_folder_id=u"inbox", folder_id=None):
-	  if folder_id:
-	  	root = 	  root = M.FindItem(
-				    {u'Traversal': u'Shallow'},
-				    M.ItemShape(T.BaseShape(format)),
-				    M.IndexedPageItemView({u'MaxEntriesReturned': u'10', u'BasePoint': u'Beginning', u'Offset': u'0'}),
-				    M.ParentFolderIds(T.FolderId(Id=folder_id)),
-				    )
-	  else:
-		root = M.FindItem(
-		    {u'Traversal': u'Shallow'},
-		    M.ItemShape(T.BaseShape(format)),
-		    M.ParentFolderIds(T.DistinguishedFolderId(Id=d_folder_id)),
-		    )
-	  return root
+    # should be in soap_request
+    def get_folder_items(self, format=u"Default", d_folder_id=u"inbox", folder_id=None):
+      if folder_id:
+        root =    root = M.FindItem(
+                    {u'Traversal': u'Shallow'},
+                    M.ItemShape(T.BaseShape(format)),
+                    M.IndexedPageItemView({u'MaxEntriesReturned': u'10', u'BasePoint': u'Beginning', u'Offset': u'0'}),
+                    M.ParentFolderIds(T.FolderId(Id=folder_id)),
+                    )
+      else:
+        root = M.FindItem(
+            {u'Traversal': u'Shallow'},
+            M.ItemShape(T.BaseShape(format)),
+            M.ParentFolderIds(T.DistinguishedFolderId(Id=d_folder_id)),
+            )
+      return root
 
-	def get_folder_items_range(self, format=u"Default", d_folder_id=u"inbox", folder_id=None, max_entries=None, offset=None):
-		if folder_id:
-			folder = T.FolderId(Id=folder_id)
-	 	else:
-	  	    folder = T.DistinguishedFolderId(Id=d_folder_id)
+    def get_folder_items_range(self, format=u"Default", d_folder_id=u"inbox", folder_id=None, max_entries=None, offset=None):
+        if folder_id:
+            folder = T.FolderId(Id=folder_id)
+        else:
+            folder = T.DistinguishedFolderId(Id=d_folder_id)
 
-	  	root = M.FindItem({u'Traversal': u'Shallow'},
-			    M.ItemShape(T.BaseShape(format)),
-			    M.IndexedPageItemView({u'MaxEntriesReturned': unicode(max_entries), u'BasePoint': u'Beginning', u'Offset': unicode(offset)}),
-			    folder,
-			    )
+        root = M.FindItem({u'Traversal': u'Shallow'},
+                M.ItemShape(T.BaseShape(format)),
+                M.IndexedPageItemView({u'MaxEntriesReturned': unicode(max_entries), u'BasePoint': u'Beginning', u'Offset': unicode(offset)}),
+                folder,
+                )
         return root
 
 
-	def get_child_folders(self, folder_id, format=u"Default"):
-		root = M.FindFolder(
-			{u'Traversal': u'Shallow'},
-			M.FolderShape(
-				T.BaseShape(format)),
-			M.ParentFolderIds(
-				T.FolderId(Id=folder_id)))
-		return root
+    def get_child_folders(self, folder_id, format=u"Default"):
+        root = M.FindFolder(
+            {u'Traversal': u'Shallow'},
+            M.FolderShape(
+                T.BaseShape(format)),
+            M.ParentFolderIds(
+                T.FolderId(Id=folder_id)))
+        return root
 
 
-	def get_distinguished_folder_id(self, d_folder_id, format=u"Default"):
-		root = M.GetFolder(
-			M.FolderShape(T.BaseShape(format)),
-			M.FolderIds(T.DistinguishedFolderId(Id=d_folder_id)))
-		try:
-			resp = self.service.send(root)
-		except pyexchange.FailedExchangeException:
-			print "Error with Exchange, perhaps there is no such folder as {}.".format(d_folder_id)
-			return None
-		return resp.xpath('//t:FolderId', namespaces=NAMESPACES)[0].get('Id')
+    def get_distinguished_folder_id(self, d_folder_id, format=u"Default"):
+        root = M.GetFolder(
+            M.FolderShape(T.BaseShape(format)),
+            M.FolderIds(T.DistinguishedFolderId(Id=d_folder_id)))
+        try:
+            resp = self.service.send(root)
+        except pyexchange.FailedExchangeException:
+            print "Error with Exchange, perhaps there is no such folder as {}.".format(d_folder_id)
+            return None
+        return resp.xpath('//t:FolderId', namespaces=NAMESPACES)[0].get('Id')
 
-	def find_folder_named(self, folder_name, ptree=False):
-		# first find the root folder
-		rootfolder_id = self.get_distinguished_folder_id('root')
-		print "Root folder: {}".format(rootfolder_id)
+    def find_folder_named(self, folder_name, ptree=False):
+        # first find the root folder
+        rootfolder_id = self.get_distinguished_folder_id('root')
+        print "Root folder: {}".format(rootfolder_id)
 
 
-		def search_children(folder_id, depth=0, ptree=False):
-			child_folders = self.service.send(self.get_child_folders(folder_id))
-			#ipdb.set_trace()
-			for folder in child_folders.xpath('//t:Folder', namespaces=NAMESPACES):
-				if ptree:
-					print "-" * depth + "{}".format(folder.xpath('t:DisplayName', namespaces=NAMESPACES)[0].text)
-				# is it our folder?
-				if folder.xpath('t:DisplayName', namespaces=NAMESPACES)[0].text == folder_name:
-					print "Found Folder: {} ({})".format(folder_name, folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'))
-					ipdb.set_trace()
-					pxml(folder.xpath('t:TotalCount', namespaces=NAMESPACES)[0])
-					return (folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'),
-							folder.xpath('t:TotalCount', namespaces=NAMESPACES)[0].text)
-				# does it have child folders?
-				elif int(folder.xpath('t:ChildFolderCount', namespaces=NAMESPACES)[0].text) > 0:
-					chain_id = search_children(folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'), depth+1, ptree)
-					if chain_id:
-						return chain_id
-			return None
-		
-		# then iterate through each folder recursively until you find the right name
-		return search_children(rootfolder_id, ptree=ptree)
+        def search_children(folder_id, depth=0, ptree=False):
+            child_folders = self.service.send(self.get_child_folders(folder_id))
+            #ipdb.set_trace()
+            for folder in child_folders.xpath('//t:Folder', namespaces=NAMESPACES):
+                if ptree:
+                    print "-" * depth + "{}".format(folder.xpath('t:DisplayName', namespaces=NAMESPACES)[0].text)
+                # is it our folder?
+                if folder.xpath('t:DisplayName', namespaces=NAMESPACES)[0].text == folder_name:
+                    print "Found Folder: {} ({})".format(folder_name, folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'))
+                    ipdb.set_trace()
+                    pxml(folder.xpath('t:TotalCount', namespaces=NAMESPACES)[0])
+                    return (folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'),
+                            folder.xpath('t:TotalCount', namespaces=NAMESPACES)[0].text)
+                # does it have child folders?
+                elif int(folder.xpath('t:ChildFolderCount', namespaces=NAMESPACES)[0].text) > 0:
+                    chain_id = search_children(folder.xpath('t:FolderId', namespaces=NAMESPACES)[0].get('Id'), depth+1, ptree)
+                    if chain_id:
+                        return chain_id
+            return None
+        
+        # then iterate through each folder recursively until you find the right name
+        return search_children(rootfolder_id, ptree=ptree)
 
-	def get_folder_xml(self, folder, format=u"Default"):
-		root = M.GetFolder(
-			M.FolderShape(T.BaseShape(format)),
-			M.FolderIds(T.DistinguishedFolderId(Id=folder))
-			)
-		return root
+    def get_folder_xml(self, folder, format=u"Default"):
+        root = M.GetFolder(
+            M.FolderShape(T.BaseShape(format)),
+            M.FolderIds(T.DistinguishedFolderId(Id=folder))
+            )
+        return root
 
-	def get_item_nck(self, id, format=u"Default"):
-		root = M.GetItem(
-			M.ItemShape(T.BaseShape(format),
-						T.IncludeMimeContent("true")),
-			M.ItemIds(
-	            T.ItemId({'Id': id})))
-		return root
+    def get_item_nck(self, id, format=u"Default"):
+        root = M.GetItem(
+            M.ItemShape(T.BaseShape(format),
+                        T.IncludeMimeContent("true")),
+            M.ItemIds(
+                T.ItemId({'Id': id})))
+        return root
  
 
-	# should be in soap_request
-	# I might be able to do this with multiple itemids, which would speed things up
-	def get_item(self, id, change_key, format=u"Default"):
-		root = M.GetItem(
-			M.ItemShape(T.BaseShape(format),
-						T.IncludeMimeContent("true")),
-			M.ItemIds(
-	            T.ItemId({'Id': id, 'ChangeKey': change_key})))
-		return root
+    # should be in soap_request
+    # I might be able to do this with multiple itemids, which would speed things up
+    def get_item(self, id, change_key, format=u"Default"):
+        root = M.GetItem(
+            M.ItemShape(T.BaseShape(format),
+                        T.IncludeMimeContent("true")),
+            M.ItemIds(
+                T.ItemId({'Id': id, 'ChangeKey': change_key})))
+        return root
 
 
-	def get_items(self, ids, format=u"Default"):
-		itemids = [T.ItemId({'Id': id, 'ChangeKey': change_key}) for id, change_key in ids]
-		root = M.GetItem(
-			M.ItemShape(T.BaseShape(format),
-						T.IncludeMimeContent("true")),
-			*itemids)
-		return root
+    def get_items(self, ids, format=u"Default"):
+        itemids = [T.ItemId({'Id': id, 'ChangeKey': change_key}) for id, change_key in ids]
+        root = M.GetItem(
+            M.ItemShape(T.BaseShape(format),
+                        T.IncludeMimeContent("true")),
+            *itemids)
+        return root
 
 
-	def process_items(self, msgxml):
-		msgs = msgxml.xpath(u'//m:ResponseMessages/m:GetItemResponseMessage', namespaces=NAMESPACES)
-		out_messages = []
-		for msg in msgs:
-			if msg.get('ResponseClass') == 'Success':
-				out_messages.append(xmltodict.parse(etree.tostring(msg)))
-		return out_messages
+    def process_items(self, msgxml):
+        msgs = msgxml.xpath(u'//m:ResponseMessages/m:GetItemResponseMessage', namespaces=NAMESPACES)
+        out_messages = []
+        for msg in msgs:
+            if msg.get('ResponseClass') == 'Success':
+                out_messages.append(xmltodict.parse(etree.tostring(msg)))
+        return out_messages
 
-		
-	def get_folder_generator(self, folder_name, distinguished_folder=True, step=50):
-		if distinguished_folder:
-			pass
-		else:
-			folder = self.find_folder_named(folder_name)
-			if not folder:
-				raise Exception("Couldn't find folder named %s" % folder_name)
-			folder_id, item_count = folder
+        
+    def get_folder_generator(self, folder_name, distinguished_folder=True, step=50):
+        if distinguished_folder:
+            pass
+        else:
+            folder = self.find_folder_named(folder_name)
+            if not folder:
+                raise Exception("Couldn't find folder named %s" % folder_name)
+            folder_id, item_count = folder
 
-		for i in xrange(int(item_count) / step):
-			# get the list of step items
-			body = self.get_folder_items_range(folder_id=folder_id, format=u"IdOnly",
-				max_entries=step, offset=i * step)
-			raw_messages = self.service.send(body)
-			items = raw_messages.xpath(u'//m:FindItemResponseMessage/m:RootFolder/t:Items/t:Message', namespaces=NAMESPACES)
-			ids = items[0].xpath('//t:ItemId', namespaces=NAMESPACES)
-			for id in ids:
-				yield MessageItem(folder_id or folder_name, id.get('Id'), self)
+        for i in xrange(int(item_count) / step):
+            # get the list of step items
+            body = self.get_folder_items_range(folder_id=folder_id, format=u"IdOnly",
+                max_entries=step, offset=i * step)
+            raw_messages = self.service.send(body)
+            items = raw_messages.xpath(u'//m:FindItemResponseMessage/m:RootFolder/t:Items/t:Message', namespaces=NAMESPACES)
+            ids = items[0].xpath('//t:ItemId', namespaces=NAMESPACES)
+            for id in ids:
+                yield MessageItem(folder_id or folder_name, id.get('Id'), self)
 
-	def get_folder(self, folder_name, distinguished_folder=True):
-		if distinguished_folder:
-			body = self.get_folder_items(d_folder_id=folder_name, format=u"IdOnly")
-		else:
-			folder_id, item_count = self.find_folder_named(folder_name)
-			body = self.get_folder_items(folder_id=folder_id, format=u"IdOnly")
-		pxml(body)
-		# print "Sending request for folder..."
-		raw_messages = self.service.send(body)
-		items = raw_messages.xpath(u'//m:FindItemResponseMessage/m:RootFolder/t:Items/t:Message', namespaces=NAMESPACES)
-		ids = items[0].xpath('//t:ItemId', namespaces=NAMESPACES)
-		msgs = []
-		for id in ids:
-			tid = id.get('Id')
-			tidck = id.get('ChangeKey')
-			root = self.get_item(tid, tidck, format=u"AllProperties")
-			# print "Sending request for message {}".format(tid)
-			msgxml = self.service.send(root)
-			msgs.append(msgxml)
-		return msgs
+    def get_folder(self, folder_name, distinguished_folder=True):
+        if distinguished_folder:
+            body = self.get_folder_items(d_folder_id=folder_name, format=u"IdOnly")
+        else:
+            folder_id, item_count = self.find_folder_named(folder_name)
+            body = self.get_folder_items(folder_id=folder_id, format=u"IdOnly")
+        pxml(body)
+        # print "Sending request for folder..."
+        raw_messages = self.service.send(body)
+        items = raw_messages.xpath(u'//m:FindItemResponseMessage/m:RootFolder/t:Items/t:Message', namespaces=NAMESPACES)
+        ids = items[0].xpath('//t:ItemId', namespaces=NAMESPACES)
+        msgs = []
+        for id in ids:
+            tid = id.get('Id')
+            tidck = id.get('ChangeKey')
+            root = self.get_item(tid, tidck, format=u"AllProperties")
+            # print "Sending request for message {}".format(tid)
+            msgxml = self.service.send(root)
+            msgs.append(msgxml)
+        return msgs
 
-	def get_archive(self):
-		return self.get_folder(u"Archive", distinguished_folder=False)
+    def get_archive(self):
+        return self.get_folder(u"Archive", distinguished_folder=False)
 
-	def get_inbox(self):
-		return self.get_folder(u"inbox")
+    def get_inbox(self):
+        return self.get_folder(u"inbox")
